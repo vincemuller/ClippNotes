71dcb7503049c43517ff97a2e1b2477130e7d9fc
vincemuller <vmuller2529@gmail.com>
2025-04-08 08:23:29 -0700

    Initial Commit

---
+++ ClippNotes/ClippNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippNotesApp.swift
+//  ClippNotes
+//
+//  Created by Vince Muller on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ClippNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
